4e9f2debaaeae6ef6f45b5cf90fe77673824da30
Zack Nelson <Zacknelson1@gmail.com>
2011-05-20 18:41:48 -0400

    Removed 006 writes

--- mem-match_sr.asm
+++ mem-match_sr.asm
@@ -4,11 +4,6 @@
 
 ;----- Load Palette Data -----;
 LOAD_PALETTE:
-	LDX $2002			; Read the PPU status to reset the latch
-	LDA #$3F
-	STA $2006			; Set the low byte $3F(00) of the address
-	LDA #$00
-	STA $2006			; Set the high byte of $(3F)00 of the address
 	LDX #$00
 LOAD_PALETTE1:
 	LDA palette, x
@@ -20,11 +15,6 @@ LOAD_PALETTE1:
 
 ;----- Load Attribute Data -----;
 LOAD_ATTRIBUTE
-	LDA $2002			; Read PPU status to reset the latch
-	LDA #$23
-	STA $2006			; Write high byte $(23)C0 of address
-	LDA #$C0
-	STA $2006			; Write low byte $23(C0) of address
 	LDX #$00
 LOAD_ATTRIBUTE0:
 	LDA attribute, x
