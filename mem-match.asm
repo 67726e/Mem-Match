3344f9bc0f9596e64c1259bcf7e3e9e4ddf52959
Glenn Nelson <glenn@hexcoder.us>
2011-05-20 17:20:57 -0400

    Update README, fix asm files to fit naming conventions

--- mem-match.asm
+++ mem-match.asm
@@ -45,20 +45,48 @@ VWAIT2:
 	BIT $2002
 	BPL VWAIT2
 
+;----- Load Palette Data -----;
+	LDX $2002			; Read the PPU status to reset the latch
+	LDA #$3F
+	STA $2006			; Set the low byte $3F(00) of the address
+	LDA #$00
+	STA $2006			; Set the high byte of $(3F)00 of the address
+	LDX #$00
+LOAD_PALETTE:
+	LDA PALETTE, x
+	STA $2007
+	INX
+	CPX #$20
+	BNE LOAD_PALETTE
+	
+;----- Load Attribute Data -----;
+	LDA $2002			; Read PPU status to reset the latch
+	LDA #$23
+	STA $2006			; Write high byte $(23)C0 of address
+	LDA #$C0
+	STA $2006			; Write low byte $23(C0) of address
+	LDX #$00
+LOAD_ATTRIBUTE:
+	LDA Attribute
+	STA $2007
+	INX
+	CPX #$40
+	BNE LOAD_ATTRIBUTE
+	
 ;----- Start Menu -----;
-STARTMENU:
+START_MENU:
 	.include "mem-match_startmenu.asm"
 
 ;----- Game Loop -----;
-GAMELOOP:	
+GAME_LOOP:	
 	.include "mem-match_gameloop.asm"
 	
 ;----- Game Over -----;
-GAMEOVER:	
+GAME_OVER:	
 	.include "mem-match_gameover.asm"
 	
 ;----- Pause Menu -----;
-PAUSEMENU:
+PAUSE_MENU:
 	.include "mem-match_pausemenu.asm"
 	
 
@@ -69,10 +97,12 @@ NMI:
 	LDA #$02
 	STA $4014			; Store high byte $(02)00 of RAM address
 
+	INC timer			; Set the flag to allow another module iteration
+	
 	RTI					; NMI Over, Return
 
 ;----- Generic Data -----;
-	.include "mem-test_data.asm"
+	.include "mem-match_data.asm"
 	
 ;----- Setup Addresses -----;
 	.org $FFFA
--- mem-match_variables.asm
+++ mem-match_variables.asm
@@ -2,3 +2,4 @@
 ;;; Declare & Initiate all variables in this file
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	.rsset $0000
+timer		.rs 1			; Holds the flag used to determine if a loop can continue iterate
