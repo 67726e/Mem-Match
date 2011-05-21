9bc4309457d469a72f39f9af8ab4ab4a0333b12a
Glenn Nelson <glenn@hexcoder.us>
2011-05-20 21:37:01 -0400

    Add macro file

--- mem-match.asm
+++ mem-match.asm
@@ -46,13 +46,6 @@ VWAIT2:
 	bit $2002
 	bpl VWAIT2
 
-	.macro ld_point	;loads a pointer source, destination
-		lda low(\1)
-		sta \2
-		lda high(\1)
-		sta \2 + 1
-	.endm
-
 	ld_point name_table_file, name_table
 	jsr LOAD_NAME_TABLE_0
 
